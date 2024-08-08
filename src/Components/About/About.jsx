34d3604c1d171e216b9c610fd4e5d2d75b985436
GuilleCozzi <109187041+GEdgardoFernandez@users.noreply.github.com>
2024-08-08 11:38:48 -0300

    Se agregaron estilos al componente About

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,17 +7,20 @@ const About = () => {
     const [activeModal, setActiveModal] = useState(null);
     const closeModal = () => setActiveModal(null);
     const navigate = useNavigate();
+    const [isOn, setIsOn] = useState(false);
+    const handleClick = () => setIsOn(!isOn);
     const fullText = "Soy un Desarrollador Full Stack con una sólida formación autodidacta y continua. Poseo un profundo conocimiento en diagnóstico y solución de problemas técnicos en software y hardware, así como una comprensión detallada de sistemas operativos, aplicaciones, drivers, componentes de hardware y configuraciones de red. Inicié mi carrera en programación con el curso de Responsive Web Design en FreeCodeCamp, adquiriendo conocimientos en HTML, CSS y JavaScript. Luego, participé en el curso de Primeros Pasos en el Desarrollo Frontend de Argentina Programa 4.0, donde reforcé y mejoré mis habilidades. Posteriormente, completé el curso de Programador Full Stack en los proyectos T. Tec. San Juan, profundizando en Java, Spring y SQL, y desarrollando habilidades blandas clave como la comunicación efectiva y el trabajo en equipo. Actualmente, estoy graduado del curso Full Stack Developer en Henry, donde adquirí nuevos leguajes y herramientas para mejorar mi stack (React, Node.js, PostgreSQL, Axios, Express, Boostrap NPM, Redux, etc), y fortaleciendo habilidades blandas como paciencia, puntualidad, organización, trabajo en equipo, resolución de problemas, etc. Mis habilidades blandas me permiten colaborar en proyectos de alto rendimiento y compartir conocimientos de manera efectiva. Estoy altamente motivado y emocionado por la industria de software. Mi objetivo a largo plazo es contribuir al desarrollo de software en empresas de prestigio global. Mi constante deseo de aprender y mejorar mis habilidades me mantiene al día con las últimas tendencias y tecnologías en el mundo del desarrollo web y software."
     return (
         <div className={styles.container}>
             <div className={styles.about}>
-                <h2>Sobre mi</h2>
-                <p>{fullText} </p>
+                <span className={styles.title}>Encender</span>
+                <input type="checkbox" className={styles.check} onClick={handleClick}/>
+                <p className={`${styles.text} ${isOn ? styles.encendido : styles.tenue}`}>{fullText}</p>
             </div>
             <div>
                 <button onClick={() => navigate('/home')} className={styles.button}>Volver</button>
             </div>
-            <div>
+            <div className={styles.containerButtons}>
                 <button onClick={() => setActiveModal('courses')} className={styles.button}>Certificados</button>
 
                 <Courses show={activeModal === 'courses'} onClose={closeModal} />
--- src/Views/About.jsx
+++ src/Views/About.jsx
@@ -1,10 +1,17 @@
 import About from "../Components/About/About";
-
+import Menu from "../Components/Menu/Menu";
+import Footer from "../Components/Footer/Footer";
 const AboutView = () => {
 
     return (
         <div>
+            <div>
+                <Menu />
+            </div>
             <About />
+            <div>
+                <Footer />
+            </div>
         </div>
     )
 }
