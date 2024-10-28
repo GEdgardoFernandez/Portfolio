b39691b716a18934d382dfda826478414498b3d0
GuilleCozzi <109187041+GEdgardoFernandez@users.noreply.github.com>
2024-10-28 16:15:06 -0300

    se agrego la parte responsiva de la mayor parte de la pagina

--- src/Components/ModalDesc/Modal.jsx
+++ src/Components/ModalDesc/Modal.jsx
@@ -5,17 +5,20 @@ const Modal = ({ show, onClose, children }) => {
   if (!show) {
     return null;
   }
-  
+
   return (
     <div className={styles.modalOverlay}>
       <div className={styles.modal}>
         <button className={styles.closeButton} onClick={onClose}>X</button>
         <div className={styles.modalContent}>
-          {children}
+          <div className={styles.carousel}>
+            {React.Children.map(children, (child, index) =>
+            (<div key={index} className={styles.carouselItem}> {child}
+            </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default Modal;
+}; export default Modal;
